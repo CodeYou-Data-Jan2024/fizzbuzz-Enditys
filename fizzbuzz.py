86661e3b09275f89295779ecc0f2c561daf27983
Justin Rice <rijust@aol.com>
2024-03-24 11:13:15 -0400

    Update to code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,9 @@
-# add your code here
-
+for num in range(1,101):
+    outcome = ""
+    if num % 3 == 0:
+        outcome = outcome + "Fizz"
+    if num % 5 == 0:
+        outcome = outcome + "Buzz"
+    if num % 5 != 0 and num % 3 != 0:
+        outcome = outcome + str(num)
+    print(outcome)
